259304420d83eebf62678fa6ddf05ba2ec2c4922
Urbestestfriend1 <littleblockbldr@gmail.com>
2026-03-22 20:29:22 -0400

    Implement colorDistance function
    Add a function to calculate color distance between two RGB colors.

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let originY = 0;
 let isDragging = false;
 let startX, startY;
 
+function colorDistance(c1, c2) {
+    return Math.sqrt(
+        (c1[0] - c2[0]) ** 2 +
+        (c1[1] - c2[1]) ** 2 +
+        (c1[2] - c2[2]) ** 2
+    );
+}
+
 map.addEventListener("wheel", (e) => {
     e.preventDefault();
 
