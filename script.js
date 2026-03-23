033bf7efbcd8d0df205fe1b292f7692da318c7cf
Urbestestfriend1 <littleblockbldr@gmail.com>
2026-03-22 20:23:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const nations = {
+    "255,0,0": {
+        name: "ALTA",
+        allies: ["Nation B"],
+        rivals: ["Nation C"],
+        population: "120,000",
+        provinces: 5,
+        tech: 1,
+        stability: "Max"
+    },
+
+    "0,255,0": {
+        name: "Nation B",
+        allies: ["ALTA"],
+        rivals: [],
+        population: "80,000",
+        provinces: 3,
+        tech: 1,
+        stability: "Max"
+    }
+};
